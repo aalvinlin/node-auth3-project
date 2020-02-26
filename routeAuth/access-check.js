7ba6a28ec639268cb36464211b8fb1a7bf0810c0
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-26 15:33:48 -0800

    wrote access-check middleware function

--- routeAuth/access-check.js
+++ routeAuth/access-check.js
@@ -4,6 +4,22 @@ const jwtSecret = require("../data/imports/secrets").jwtSecret;
 
 function accessCheck (req, res, next) {
 
+    const { authorization } = req.headers;
+
+    if (!authorization)
+        { res.status(400).json({ message: "No credentials provided."})}
+    else
+        {
+            jwt.verify(authorization, jwtSecret, (error, decodedToken) => {
+                if (error)
+                    { res.status(401).json({ message: "Invalid credentials."})}
+                else
+                    {
+                        req.decodedToken = decodedToken;
+                        next();
+                    }
+            })
+        }
 }
 
 module.exports(accessCheck);
